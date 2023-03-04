1ed15b6c63194f2b62ded910b42730cf930b1d9a
Javier <ingenierodesoftware73@gmail.com>
2023-03-04 17:07:48 -0500

    first class created named people

--- 02-constructs/coche.php
+++ 02-constructs/coche.php
@@ -1,61 +1,67 @@
 <?php
 
-class Coche{
+class Coche
+{
     public $placa;
     private $color;
     protected $velocidad;
     public $modelo;
 
-    public function __construct($placa, $color, $velocidad, $modelo){
+    public function __construct($placa, $color, $velocidad, $modelo)
+    {
         $this->placa = $placa;
         $this->color = $color;
         $this->velocidad = $velocidad;
         $this->modelo = $modelo;
     }
 
-    public function getColor(){
+    public function getColor()
+    {
         return $this->color;
     }
 
-    public function setColor($color){
+    public function setColor($color)
+    {
         $this->color = $color;
     }
 
-    public function acelerar(){
+    public function acelerar()
+    {
         $this->velocidad++;
     }
 
-    public function frenar(){
+    public function frenar()
+    {
         $this->velocidad--;
     }
 
-    public function getVelocidad(){
+    public function getVelocidad()
+    {
         return $this->velocidad;
     }
 
-    public function setModelo($modelo){
+    public function setModelo($modelo)
+    {
         $this->modelo = $modelo;
     }
 
-    public function getModelo(){
+    public function getModelo()
+    {
         return $this->modelo;
     }
 
-    public function showInformation(coche $myObject){
-        if(is_object($myObject)){
+    public function showInformation(coche $myObject)
+    {
+        if (is_object($myObject)) {
             $information = "<h1>The Cars information</h1><br>";
-            $information .= "<h3>Placa: ".$myObject->placa."</h3>";
-            $information .= "<h3>Color: ".$myObject->color."</h3>";
-            $information .= "<h3>Velocidad: ".$myObject->velocidad."</h3>";
-            $information .= "<h3>Modelo: ".$myObject->modelo."</h3>";
+            $information .= "<h3>Placa: " . $myObject->placa . "</h3>";
+            $information .= "<h3>Color: " . $myObject->color . "</h3>";
+            $information .= "<h3>Velocidad: " . $myObject->velocidad . "</h3>";
+            $information .= "<h3>Modelo: " . $myObject->modelo . "</h3>";
 
             return $information;
-        }else{
-            echo "The data is ".$myObject;
+        } else {
+            echo "The data is " . $myObject;
         }
     }
 }
-
-
-
-
---
+++ 03-herencia/classes.php
@@ -0,0 +1,111 @@
+<?php
+
+class People{
+    public $name;
+    public $age;
+    public $eight;
+    public $genre;
+
+
+
+    /**
+     * Get the value of name
+     */ 
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @return  self
+     */ 
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of age
+     */ 
+    public function getAge()
+    {
+        return $this->age;
+    }
+
+    /**
+     * Set the value of age
+     *
+     * @return  self
+     */ 
+    public function setAge($age)
+    {
+        $this->age = $age;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of eight
+     */ 
+    public function getEight()
+    {
+        return $this->eight;
+    }
+
+    /**
+     * Set the value of eight
+     *
+     * @return  self
+     */ 
+    public function setEight($eight)
+    {
+        $this->eight = $eight;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of genre
+     */ 
+    public function getGenre()
+    {
+        return $this->genre;
+    }
+
+    /**
+     * Set the value of genre
+     *
+     * @return  self
+     */ 
+    public function setGenre($genre)
+    {
+        $this->genre = $genre;
+
+        return $this;
+    }
+
+    /**
+     * Function to show the method talk with return
+     *
+     * @return some text
+     */
+    public function talk(){
+        return "Is tolking";
+    }
+
+
+    /**
+     * Function walk to show some text
+     *
+     * @return some text
+     */
+    public function walk(){
+        return "Is walking";
+    }
+
+
+}
