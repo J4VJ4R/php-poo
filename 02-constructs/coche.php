b89b1680275fd3491cb3acad762aa639509d1c18
Javier <ingenierodesoftware73@gmail.com>
2023-03-03 20:54:56 -0500

    Construct with five cars

--- 02-constructs/coche.php
+++ 02-constructs/coche.php
@@ -1,11 +1,17 @@
 <?php
 
-class car{
-    public $placa = "abc-532";
-    public $color = "Rojo";
-    public $velocidad = 300;
-    public $modelo = "mazda";
+class Coche{
+    public $placa;
+    public $color;
+    public $velocidad;
+    public $modelo;
 
+    public function __construct($placa, $color, $velocidad, $modelo){
+        $this->placa = $placa;
+        $this->color = $color;
+        $this->velocidad = $velocidad;
+        $this->modelo = $modelo;
+    }
 
     public function getColor(){
         return $this->color;
@@ -36,21 +42,6 @@ class car{
     }
 }
 
-$car1 = new car();
 
 
 
-$car1->setColor("amarillo");
-echo "<hr>";
-echo "El color de carro 1 es: ".$car1->getColor()."<br>";
-echo "El modelo de carro 1 es: ".$car1->getModelo();
-
-
-echo "<hr>";
-
-$car2 = new car();
-$car2->setColor("Verde");
-$car2->setModelo("Kia");
-
-echo "el color del coche 2 es: ".$car2->getColor()."<br>";
-echo "El modelo de carro 2 es: ".$car2->getModelo()."<br>";
